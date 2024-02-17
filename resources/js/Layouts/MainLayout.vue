733ad804a912c3025e3bbe74a86941a982dca210
gsokolowski <gregor.sokolowski@gmail.com>
2024-02-17 00:55:37 +0000

    Listing route and inertia index and show Listing

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3"; //not inertia-vue3
 import { ref } from "vue";
 const timer = ref(0);
 setInterval(() => timer.value++, 1000);
